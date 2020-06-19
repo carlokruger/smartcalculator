ea53ad8378928a48319b5955a6fc26518de77f27
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-06-19 16:42:55 +1200

    incomplete refactor to deal with variables

--- calculator.py
+++ calculator.py
@@ -2,6 +2,42 @@
 
 running_total = 0
 nums = 0
+variables = {}
+equal_count = 0
+
+def commands(text):
+    if text[0] == "/exit":
+        print("Bye!")
+        break
+
+    elif text[0] == "/help":
+        print("The program calculates the output of an expression")
+
+    elif text[0][0] == "/":
+        print("Unknown command")
+
+def return_variable(text):
+    if not text.isalpha():
+        print("Invalid identifier")
+
+    elif text in variables:
+        print(variables[text])
+
+    else:
+        print("Unknown variable")
+
+def return_single_digit(text):
+    try:
+        running_total += int(text[0])
+
+    except ValueError:
+        print("Invalid identifier")
+
+    else:
+        print(running_total)
+
+def variable_assignment(text):
+    pass
 
 while True:
     text_in = input().split()
@@ -9,29 +45,23 @@ while True:
     if len(text_in) == 0:
         pass
 
-    elif len(text_in) == 1:
-        if text_in[0] == "/exit":
-            print("Bye!")
-            break
+    elif len(text_in) == 1 and text_in[0][0] == "/":
+        commands(text_in)
 
-        elif text_in[0] == "/help":
-            print("The program calculates the output of an expression")
+    elif len(text_in) == 1 and text_in[0][0].isalpha():
+        return_variable(text_in)
 
-        elif text_in[0][0] == "/":
-            print("Unknown command")
-
-        elif text_in[0][0] != "/":
-            try:
-                running_total += int(text_in[0])
-
-            except ValueError:
-                print("Invalid expression1")
-
-            else:
-                print(running_total)
+    elif len(text_in) == 1 and text_in[0][0] != "/":
+        return_single_digit(text_in)
 
     elif len(text_in) > 1 and len(text_in) % 2 == 0:
-        print("Invalid expression2")
+        for x in text_in:
+            if "=" in x:
+                equal_count += 1
+        if equal_count == 1:
+            variable_assignment(text_in)
+        else:
+            print("Invalid expression2")
 
     else:
         try:
